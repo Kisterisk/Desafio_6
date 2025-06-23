a87d6e34e5b3b5b1eb46c222cdeba07f8e2b69f1
Kisterisk <jefi9724@gmail.com>
2025-06-22 20:46:29 -0400

    Resolvimos el ejercicio C

--- parte c/js/main.js
+++ parte c/js/main.js
@@ -1,2 +1,16 @@
 var ejercicioC = 'Ejercicio C'
 console.warn(ejercicioC)
+
+var cantidadDeGatos = 10;
+var cantidadDePasos = 6;
+
+for (var gato = 1; gato <= cantidadDeGatos; gato++) {
+    var pasos = "";
+    for (var paso = 1; paso <= cantidadDePasos; paso++) {
+        pasos += "🐾";
+    }
+
+    var emojiGato = (gato % 2 === 1) ? "🐈" : "🐈⬛";
+
+    console.log("Gato #" + gato + ": " + emojiGato + " " + pasos);
+}
